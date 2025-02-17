7c8e9eb5223a46ef88da2679daed4b17aca24873
maryunnie <mariannygoncalvesmoreira@gmail.com>
2025-02-17 14:29:33 -0400

    UP

--- src/sevicos/conexao.js
+++ src/sevicos/conexao.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'libertadores',
-    password: '',
-    database: ''
+    user: 'mary',
+    password: '12345',
+    database: 'L'
 });
 
 export default pool;
